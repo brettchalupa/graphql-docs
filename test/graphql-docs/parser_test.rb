4191e969e5f436d8e28a823dbafaf7e159438ae3
Garen Torikian <gjtorikian@gmail.com>
2017-02-20 22:58:23 -0800

    Ensure mutationless schemas do not explode

--- test/graphql-docs/parser_test.rb
+++ test/graphql-docs/parser_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class ParserTest < Minitest::Test
   def setup
-    @json = File.read(File.join(fixtures_dir, 'gh-api.json'))
-    @parser = GraphQLDocs::Parser.new(@json, {})
+    @ghapi = File.read(File.join(fixtures_dir, 'gh-api.json'))
+    @swapi = File.read(File.join(fixtures_dir, 'sw-api.json'))
+    @parser = GraphQLDocs::Parser.new(@ghapi, {})
     @results = @parser.parse
     @issue = @results['types'].find { |t| t['name'] == 'Issue' }
   end
@@ -24,5 +25,13 @@ class ParserTest < Minitest::Test
     assert @results['interface_types']
     assert @results['enum_types']
     assert @results['union_types']
+    assert @results['mutation_types']
+  end
+
+  def test_mutationless_schemas_do_not_explode
+    parser = GraphQLDocs::Parser.new(@swapi, {})
+    results = parser.parse
+
+    assert_empty results['mutation_types']
   end
 end
