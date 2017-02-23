d07ff25e3ccda5e1ed1a03ea2ec7205c3fc2495d
Stephen Dotz <scdotz@gmail.com>
2017-02-22 21:58:22 -0500

    Add auth options to CLI

--- bin/graphql-docs.rb
+++ bin/graphql-docs.rb
@@ -8,11 +8,21 @@ class GraphQLDocsCLI < Thor
   include GraphQLDocs
 
   class_option :verbose, :type => :boolean
+
   desc "generate", "generate docs for a graphql API"
+
   option :url, :type => :string, :desc => "The URL of your GraphQL API"
   option :path, :type => :string, :desc => "Path to the JSON that results from the introspection query"
+
+  option :access_token, :type => :string, :desc => "Uses this token while making requests through GraphQLDocs::Client."
+  option :login, :type => :string, :desc => "Uses this login while making requests through GraphQLDocs::Client."
+  option :password, :type => :string, :desc => "Uses this password while making requests through GraphQLDocs::Client."
+
   option :output_dir, :type => :string, :desc => "Path to output generated docs", :default => File.join(Dir.pwd, 'output')
   option :delete_output, :type => :boolean, :desc => "Delete the output dir before generating", :default => false
+  option :include_assets, :type => :boolean, :desc => "Include CSS and JS assets in the output"
+
+
 
   def generate
     if options[:path].nil? && options[:url].nil?
@@ -26,6 +36,10 @@ class GraphQLDocsCLI < Thor
             path: options[:path],
             output_dir: options[:output_dir],
             delete_output: options[:delete_output],
+            include_assets: options[:include_assets],
+            access_token: options[:access_token],
+            login: options[:login],
+            password: options[:password],
         }
     )
 
